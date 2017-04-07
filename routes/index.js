f00f88b668d75f2a7a9d273f826f89a10e9d6395
Ashutosh Nigam <ashutosh.titan@gmail.com>
2017-04-07 19:16:10 +1000

    first commit

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var express = require('express');
 var braintree = require('braintree');
 var router = express.Router(); // eslint-disable-line new-cap
 var gateway = require('../lib/gateway');
+var db = require('../lib/firebasedb');
+var moment = require('moment');
 
 var TRANSACTION_SUCCESS_STATUSES = [
   braintree.Transaction.Status.Authorizing,
@@ -28,7 +30,7 @@ function formatErrors(errors) {
 
 function createResultObject(transaction) {
   var result;
-  var status = transaction.status;
+  var status = transaction !== null ?transaction.status : -1;
 
   if (TRANSACTION_SUCCESS_STATUSES.indexOf(status) !== -1) {
     result = {
@@ -48,12 +50,27 @@ function createResultObject(transaction) {
 }
 
 router.get('/', function (req, res) {
-  res.redirect('/checkouts/new');
+  res.redirect('/checkouts/landing');
 });
+router.get('/checkouts/landing', function(req,res){
+  var ref = db.ref("/");
+    ref.on("value", function(snapshot) {
+        console.log(snapshot.val());
+        res.render('checkouts/landing', {totalamount : snapshot.val().totalamount , users: snapshot.val().Users});
+      }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
+})
 
 router.get('/checkouts/new', function (req, res) {
   gateway.clientToken.generate({}, function (err, response) {
-    res.render('checkouts/new', {clientToken: response.clientToken, messages: req.flash('error')});
+    var totalamount = db.ref("/totalamount");
+    totalamount.on("value", function(snapshot) {
+        res.render('checkouts/new', {clientToken: response.clientToken, totalamount : snapshot.val(), messages: req.flash('error')});
+      }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
+    
   });
 });
 
@@ -71,12 +88,29 @@ router.post('/checkouts', function (req, res) {
   var transactionErrors;
   var amount = req.body.amount; // In production you should not take amounts directly from clients
   var nonce = req.body.payment_method_nonce;
-
+  var fn = req.body.fullName;
+  var email = req.body.email;
   gateway.transaction.sale({
     amount: amount,
-    paymentMethodNonce: nonce
+    paymentMethodNonce: nonce,
+    customer: {
+    firstName : fn,
+    email : email
+    },
+    options: {
+      storeInVaultOnSuccess: true
+    }
   }, function (err, result) {
     if (result.success || result.transaction) {
+      var totalamount = db.ref("/totalamount");
+      totalamount.transaction(function (current_value) {
+        return (current_value || 0) + parseInt(req.body.amount);
+      });
+      var userref = db.ref('/Users');
+      var newuserref = userref.push();
+      var date = moment(new Date()).format('DD/MM/YYYY MM:HH A');
+      console.log(date);
+      newuserref.set({name:req.body.fullName , email: req.body.email,amount : req.body.amount , time: date})
       res.redirect('checkouts/' + result.transaction.id);
     } else {
       transactionErrors = result.errors.deepErrors();
